5925d2553e8c7e0107da23f1536ea20f88158724
abhi <ugabhi@proton.me>
2024-10-08 16:05:51 +0530

    added shellScript

---
+++ shellScript/flake.nix
@@ -0,0 +1,23 @@
+# Ref: https://ertt.ca/nix/shell-scripts/
+{
+  description = "A simple script";
+  outputs = { self, nixpkgs }: {
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.my-script;
+
+    packages.x86_64-linux.my-script =
+      let
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in
+
+      pkgs.writeShellScriptBin "my-script" ''
+
+        DATE="$(${pkgs.ddate}/bin/ddate +'the %e of %B%, %Y')"
+
+        ${pkgs.cowsay}/bin/cowsay Hello, world! Today is $DATE.
+
+        ${pkgs.pfetch}/bin/pfetch
+
+      '';
+  };
+}
+  # nix run ./shellScript#
