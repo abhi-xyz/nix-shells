d68704471c50f7d1188dc736495cd4cf2cc45ad2
Abhinandh S <ugabhi@proton.me>
2024-11-08 11:42:15 +0530

    Refactoring

---
+++ rust-overlay/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+    unstable-nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, unstable-nixpkgs, rust-overlay }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+      overlays = [
+        rust-overlay.overlays.default
+        (final: prev: {
+          unstable = import unstable-nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        })
+      ];
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        rust-bin.stable.latest.default
+        clang
+        llvmPackages.bintools
+        unstable.neovim
+      ];
+    };
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
