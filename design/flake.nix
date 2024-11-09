59330140c6f2507edc997f29f76832750ca21def
Abhinandh S <ugabhi@proton.me>
2024-11-09 21:05:29 +0530

    Refactoring

---
+++ design/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Flake template";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+    unstable-nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, unstable-nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+      overlays = [
+        (final: prev: {
+          unstable = import unstable-nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        })
+      ];
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        unstable.neovim
+        unstable.gimp-with-plugins
+        unstable.inkscape-with-extensions
+        unstable.krita
+      ];
+      shellHook = ''
+        echo "Environment ready!" | ${pkgs.lolcat}/bin/lolcat
+      '';
+    };
+  };
+}
